6ce4e38cb6a8d984c3c9205ebb7076c9b5685cad
Rhythmic System <mc.adhdmc@gmail.com>
2025-05-12 14:57:36 -0700

    Implement more feedback messages

--- src/main/java/simplexity/simplenicks/commands/subcommands/DeleteSubCommand.java
+++ src/main/java/simplexity/simplenicks/commands/subcommands/DeleteSubCommand.java
@@ -3,18 +3,37 @@ package simplexity.simplenicks.commands.subcommands;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.command.brigadier.MessageComponentSerializer;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import simplexity.simplenicks.SimpleNicks;
 import simplexity.simplenicks.commands.NicknameProcessor;
 import simplexity.simplenicks.commands.arguments.NicknameArgument;
+import simplexity.simplenicks.config.Message;
 import simplexity.simplenicks.saving.Nickname;
 import simplexity.simplenicks.util.Constants;
 
 @SuppressWarnings("UnstableApiUsage")
 public class DeleteSubCommand implements SubCommand {
+
+    private final MiniMessage miniMessage = SimpleNicks.getMiniMessage();
+
+    private final SimpleCommandExceptionType ERROR_CANNOT_DELETE = new SimpleCommandExceptionType(
+            MessageComponentSerializer.message().serialize(
+                    miniMessage.deserialize(
+                            Message.ERROR_DELETE_FAILURE.getMessage(),
+                            Placeholder.parsed("prefix", Message.PLUGIN_PREFIX.getMessage())
+                    )
+            )
+    );
+
     @Override
     public void subcommandTo(@NotNull LiteralArgumentBuilder<CommandSourceStack> root) {
 
@@ -29,13 +48,18 @@ public class DeleteSubCommand implements SubCommand {
     }
 
     @Override
-    public int execute(@NotNull CommandContext<CommandSourceStack> ctx) {
+    public int execute(@NotNull CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
         OfflinePlayer player = (OfflinePlayer) ctx.getSource().getSender();
         Nickname nickname = ctx.getArgument("nickname", Nickname.class);
         boolean deleted = NicknameProcessor.getInstance().deleteNickname(player, nickname.nickname());
-        if (deleted) return Command.SINGLE_SUCCESS;
-        return 0;
+        if (deleted) {
+            if (player instanceof Player onlinePlayer) sendFeedback(onlinePlayer, Message.DELETE_NICK, nickname);
+            return Command.SINGLE_SUCCESS;
+        }
+        throw ERROR_CANNOT_DELETE.create();
     }
+
+
 
     @Override
     public boolean canExecute(@NotNull CommandSourceStack css) {
--- src/main/java/simplexity/simplenicks/commands/subcommands/SetSubCommand.java
+++ src/main/java/simplexity/simplenicks/commands/subcommands/SetSubCommand.java
@@ -8,30 +8,45 @@ import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.command.brigadier.MessageComponentSerializer;
-import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import simplexity.simplenicks.SimpleNicks;
 import simplexity.simplenicks.commands.NicknameProcessor;
 import simplexity.simplenicks.commands.arguments.NicknameArgument;
 import simplexity.simplenicks.config.ConfigHandler;
+import simplexity.simplenicks.config.Message;
 import simplexity.simplenicks.saving.Nickname;
 import simplexity.simplenicks.util.Constants;
 
 @SuppressWarnings("UnstableApiUsage")
 public class SetSubCommand implements SubCommand{
+    private final MiniMessage miniMessage = SimpleNicks.getMiniMessage();
 
     private final DynamicCommandExceptionType ERROR_LENGTH = new DynamicCommandExceptionType(
             nickname -> MessageComponentSerializer.message().serialize(
-                    Component.text("Nicknames must be <=" + ConfigHandler.getInstance().getMaxLength() + " characters after tags are stripped.\nYour nickname stripped is:\n" + nickname)
+                    miniMessage.deserialize(
+                            Message.ERROR_INVALID_NICK_LENGTH.getMessage(),
+                            Placeholder.unparsed("value", String.valueOf(ConfigHandler.getInstance().getMaxLength())),
+                            Placeholder.unparsed("name", nickname.toString()),
+                            Placeholder.parsed("prefix", Message.PLUGIN_PREFIX.getMessage())
+                    )
             )
     );
 
     private final DynamicCommandExceptionType ERROR_REGEX = new DynamicCommandExceptionType(
             nickname -> MessageComponentSerializer.message().serialize(
-                    Component.text("The nickname " + nickname + " is invalid.")
+                    miniMessage.deserialize(
+                            Message.ERROR_INVALID_NICK.getMessage(),
+                            Placeholder.unparsed("regex", ConfigHandler.getInstance().getRegexString()),
+                            Placeholder.parsed("prefix", Message.PLUGIN_PREFIX.getMessage())
+                    )
             )
     );
+
+
 
     @Override
     public void subcommandTo(@NotNull LiteralArgumentBuilder<CommandSourceStack> root) {
@@ -54,6 +69,7 @@ public class SetSubCommand implements SubCommand{
         if (!player.hasPermission(Constants.NICK_LENGTH_BYPASS) && nickname.normalizedNickname().length() > ConfigHandler.getInstance().getMaxLength()) throw ERROR_LENGTH.create(nickname.normalizedNickname());
         if (!player.hasPermission(Constants.NICK_REGEX_BYPASS) && !ConfigHandler.getInstance().getRegex().matcher(nickname.normalizedNickname()).matches()) throw ERROR_REGEX.create(nickname.normalizedNickname());
         NicknameProcessor.getInstance().setNickname((OfflinePlayer) ctx.getSource().getSender(), nickname.nickname());
+        sendFeedback(player, Message.CHANGED_SELF, nickname);
         return Command.SINGLE_SUCCESS;
     }
 
