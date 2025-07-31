3a60c562d6570fa0674ee175cac57c32f0b6b379
Rhythmic System <mc.adhdmc@gmail.com>
2025-07-30 21:10:24 -0700

    Attempt at a fix for the current version

--- src/main/java/simplexity/simplenicks/listener/LoginListener.java
+++ src/main/java/simplexity/simplenicks/listener/LoginListener.java
@@ -1,14 +1,16 @@
 package simplexity.simplenicks.listener;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import simplexity.simplenicks.SimpleNicks;
 import simplexity.simplenicks.util.NickHandler;
 
 public class LoginListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onPlayerLogin(PlayerJoinEvent joinEvent) {
-        NickHandler.getInstance().refreshNickname(joinEvent.getPlayer());
+        Bukkit.getScheduler().runTaskLater(SimpleNicks.getInstance(), () -> NickHandler.getInstance().refreshNickname(joinEvent.getPlayer()), 20L);
     }
 }
