343266e53bdc667f390ee31d66cad4f7788dfa6f
Peashooter101 <Peashooter101@yahoo.com>
2026-06-04 14:04:04 -0500

    Remove old src/ directory

--- src/main/java/simplexity/simplenicks/saving/NicknameRecord.java
+++
@@ -1,7 +0,0 @@
-package simplexity.simplenicks.saving;
-
-import java.util.UUID;
-
-public record NicknameRecord (UUID uuid, String username, String nickname, String normalized,
-                              boolean isActiveNickname, long lastLogin) {
-}
